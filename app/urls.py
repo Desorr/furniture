3b42378455ba4801c8d5943b446ba41c06702357
Egor <boss.markovskiy@mail.ru>
2023-12-27 00:48:54 +0300

    goods app, connected catalog & product html

--- app/urls.py
+++ app/urls.py
@@ -14,7 +14,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from tkinter.font import names
 from django.contrib import admin
 from django.urls import include, path
 
@@ -22,5 +21,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls', namespace='main'))
+    path('', include('main.urls', namespace='main')),
+    path('catalog/', include('goods.urls', namespace='catalog')),
 ]
--- main/urls.py
+++ main/urls.py
@@ -7,5 +7,5 @@ app_name = 'main'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('about', views.about, name='about'),
+    path('about/', views.about, name='about'),
 ]
