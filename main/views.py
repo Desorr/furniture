fbd61f4bec02fcf909a7eece76e4ca60ba03efe3
Egor <boss.markovskiy@mail.ru>
2023-12-27 00:18:03 +0300

    main app, pages index and about

--- main/views.py
+++ main/views.py
@@ -1,5 +1,20 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
+
 def index(request):
-    return HttpResponse('Hello')
+    context = {
+        'title': 'Home - Главная',
+        'content': "Магазин мебели HOME"
+    }
+
+    return render(request, 'main/index.html', context)
+
+
+def about(request):
+    context = {
+        'title': 'Home - О нас',
+        'content': "О нас",
+        'text_on_page': 'Новые решения в вашем доме'
+    }
+
+    return render(request, 'main/about.html', context)
