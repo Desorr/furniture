f8ddd188ea1912e1cb8df2534f3a338aa98f0bbb
Egor <boss.markovskiy@mail.ru>
2023-12-28 02:28:47 +0300

    fixtures for goods

--- goods/admin.py
+++ goods/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from goods.models import Categories, Products
+
+
+@admin.register(Categories)
+class CategoriesAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Products)
+class ProductsAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
