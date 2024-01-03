7e86f9c95d893417f297e8817f06f6ff25fa797a
Egor <boss.markovskiy@mail.ru>
2024-01-04 01:04:16 +0300

    slug for categories

--- goods/urls.py
+++ goods/urls.py
@@ -6,6 +6,6 @@ app_name = 'goods'
 
 
 urlpatterns = [
-    path('', views.catalog, name='index'),
+    path('<slug:category_slug>/', views.catalog, name='index'),
     path('product/<slug:product_slug>/', views.product, name='product'),
 ]
--- goods/views.py
+++ goods/views.py
@@ -1,11 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404, render
 
 from goods.models import Products
 
 
-def catalog(request):
+def catalog(request, category_slug):
 
-    goods = Products.objects.all()
+    if category_slug == 'all':
+        goods = Products.objects.all()
+    else:
+        goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
+    
 
     context: dict = {
         "title": "Home - Каталог",
