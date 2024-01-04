6c40bd39e84148a8e4dd6053af4f315de81e9f28
Egor <boss.markovskiy@mail.ru>
2024-01-05 00:03:08 +0300

    add search

--- goods/urls.py
+++ goods/urls.py
@@ -6,6 +6,7 @@ app_name = 'goods'
 
 
 urlpatterns = [
+    path('search/', views.catalog, name='search'),
     path('<slug:category_slug>/', views.catalog, name='index'),
     path('product/<slug:product_slug>/', views.product, name='product'),
 ]
