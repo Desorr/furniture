855239c4fa6f9562759ccf22c1cd710bf2a50f36
Egor <boss.markovskiy@mail.ru>
2024-01-05 02:17:42 +0300

    postgresql  and new search

--- goods/utils.py
+++ goods/utils.py
@@ -1,4 +1,5 @@
 from django.db.models import Q
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 from goods.models import Products
 
 
@@ -6,14 +7,19 @@ def q_search(query):
     
     if query.isdigit() and len(query) <= 5:
         return Products.objects.filter(id=int(query))
+    vector = SearchVector("name", "description")
+    query = SearchQuery(query)
     
+    return Products.objects.annotate(rank=SearchRank(vector, query)).order_by("-rank")
 
-    keywords = [word for word in query.split() if len(word) > 2]
 
-    q_objects = Q()
 
-    for token in keywords:
-        q_objects |= Q(description__icontains=token)
-        q_objects |= Q(name__icontains=token)
+    # keywords = [word for word in query.split() if len(word) > 2]
 
-    return Products.objects.filter(q_objects)
+    # q_objects = Q()
+
+    # for token in keywords:
+    #     q_objects |= Q(description__icontains=token)
+    #     q_objects |= Q(name__icontains=token)
+
+    # return Products.objects.filter(q_objects)
