b2c4bb39782501098f07771499f9514f294e7c1d
Egor <boss.markovskiy@mail.ru>
2024-01-06 01:12:37 +0300

    login form full

--- app/settings.py
+++ app/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
-from django.conf.global_settings import MEDIA_ROOT, MEDIA_URL, STATICFILES_DIRS
+from django.conf.global_settings import AUTH_USER_MODEL, MEDIA_ROOT, MEDIA_URL, STATICFILES_DIRS
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -151,3 +151,4 @@ INTERNAL_IPS =[
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'users.User'
--- users/views.py
+++ users/views.py
@@ -1,25 +1,43 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
+from users.forms import UserLoginForm
+
 
 def login(request):
+    if request.method == "POST":
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST["username"]
+            password = request.POST["password"]
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse("main:index"))
+    else:
+        form = UserLoginForm()
+
     context = {
-        'title': 'Home - Авторизация',
+        "title": "Home - Авторизация",
+        "form": form,
     }
-    return render(request, 'users/login.html', context)
+    return render(request, "users/login.html", context)
 
 
 def registration(request):
     context = {
-        'title': 'Home - Регистрация',
+        "title": "Home - Регистрация",
     }
-    return render(request, 'users/registration.html', context)
+    return render(request, "users/registration.html", context)
 
 
 def profile(request):
     context = {
-        'title': 'Home - Кабинет',
+        "title": "Home - Кабинет",
     }
-    return render(request, 'users/profile.html', context)
+    return render(request, "users/profile.html", context)
 
 
 def logout(request):
-   pass
+    pass
