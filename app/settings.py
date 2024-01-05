4c9de3f7539fb4cd628ab3e98d4db489ea4d6ab2
Egor <boss.markovskiy@mail.ru>
2024-01-05 22:01:20 +0300

    templates for users app

--- app/settings.py
+++ app/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
     'main',
     'goods',
+    'users',
 
 ]
 
@@ -65,7 +66,7 @@ ROOT_URLCONF = 'app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
