fc937c64f1759677527018a4f67fb09f974c386e
Egor <boss.markovskiy@mail.ru>
2024-01-06 01:38:44 +0300

    registration form

--- users/views.py
+++ users/views.py
@@ -1,8 +1,9 @@
+from django.contrib import auth
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.urls import reverse
 
-from users.forms import UserLoginForm
+from users.forms import UserLoginForm, UserRegistrationForm
 
 
 def login(request):
@@ -26,8 +27,20 @@ def login(request):
 
 
 def registration(request):
+
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.instance
+            auth.login(request, user)
+            return HttpResponseRedirect(reverse('main:index'))
+    else:
+        form = UserRegistrationForm()
+        
     context = {
         "title": "Home - Регистрация",
+        'form': form,
     }
     return render(request, "users/registration.html", context)
 
@@ -40,4 +53,5 @@ def profile(request):
 
 
 def logout(request):
-    pass
+    auth.logout(request)
+    return redirect(reverse('main:index'))
