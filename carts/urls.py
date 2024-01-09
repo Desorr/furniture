675da318a87f25252778942241b26457506dfe90
Egor <boss.markovskiy@mail.ru>
2024-01-09 23:23:15 +0300

    cart view and cart templates

--- carts/urls.py
+++ carts/urls.py
@@ -6,7 +6,7 @@ app_name = 'carts'
 
 
 urlpatterns = [
-    path('cart_add/<int:product_id>/', views.cart_add, name='cart_add'),
-    path('cart_change/<int:product_id>/', views.cart_change, name='cart_change'),
-    path('cart_remove/<int:product_id>/', views.cart_remove, name='cart_remove'),
+    path('cart_add/<slug:product_slug>/', views.cart_add, name='cart_add'),
+    path('cart_change/<slug:product_slug>/', views.cart_change, name='cart_change'),
+    path('cart_remove/<slug:product_slug>/', views.cart_remove, name='cart_remove'),
 ]
