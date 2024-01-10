a35f479cb347cb0591cbffbd74dbace8b137e74a
Egor <boss.markovskiy@mail.ru>
2024-01-11 01:03:57 +0300

    cart session anonymous user

--- users/views.py
+++ users/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render
 from django.urls import reverse
+from carts.models import Cart
 
 from users.forms import UserLoginForm, UserRegistrationForm, ProfileForm
 
@@ -14,9 +15,14 @@ def login(request):
             username = request.POST["username"]
             password = request.POST["password"]
             user = auth.authenticate(username=username, password=password)
+            session_key = request.session.session_key
+
             if user:
                 auth.login(request, user)
                 messages.success(request, f"{username}, Вы вошли в аккаунт")
+                
+                if session_key:
+                    Cart.objects.filter(session_key=session_key).update(user=user)
                     
                 redirect_page = request.POST.get('next', None)
                 if redirect_page and redirect_page != reverse('user:logout'):
@@ -38,8 +44,13 @@ def registration(request):
         form = UserRegistrationForm(data=request.POST)
         if form.is_valid():
             form.save()
+
+            session_key = request.session.session_key
             user = form.instance
             auth.login(request, user)
+
+            if session_key:
+                Cart.objects.filter(session_key=session_key).update(user=user)
             messages.success(
                 request,
                 f"{user.username}, Вы успешно зарегистрированы и вошли в аккаунт",
